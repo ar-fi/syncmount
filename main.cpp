2570ae33eb5cf6275d560b578c47a030ed721095
Stanislav Silnicki <gitkraken@mailgate.us>
2020-11-04 05:22:09 +0300

    AbstractLogger class established and process fork/pid/restart implemented

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <string>
 #include <map>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <tuple>
 #include <cstdarg>
+#include <sstream>
+#include <memory>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
 using namespace std::string_literals;
 
 #define DEFAULT_VALUE_SETTING "default"
@@ -24,6 +32,7 @@ using namespace std::string_literals;
 #define DEFAULT_ROOT_MOUNT "/var/syncmount"
 #define DEFAULT_MQUEUE "/syncmount"
 #define DEFAULT_LOGFILE "/var/log/syncmount.log"
+#define PID_FILE "syncmount.pid"
 
 typedef std::map<const std::string, const char *> option_settings_t;
 const std::map<const std::string, const option_settings_t> OPTIONS = {
@@ -39,6 +48,8 @@ const std::map<const std::string, const option_settings_t> OPTIONS = {
     {DAEMONIZE_OPTION, {{NO_VALUE_FLAG, ""}, {HELP_STRING_SETTING, "Daemonize (aka -b)"}}},
 };
 
+typedef std::map<const std::string, const char *> command_string_options_t;
+
 void print_map(const auto &map)
 {
     const auto print_key_value = [&](const auto &data) {
@@ -49,6 +60,40 @@ void print_map(const auto &map)
     for_each(begin(map), end(map), print_key_value);
 }
 
+class AbstractLogger
+{
+public:
+    AbstractLogger() : ready(false) {}
+    AbstractLogger(const AbstractLogger &) = delete;
+    AbstractLogger &operator=(const AbstractLogger &) = delete;
+    bool isReady() { return ready; }
+    virtual void Write(const std::string &) = 0;
+    virtual void Init(const command_string_options_t &) = 0;
+    virtual void DeInit() = 0;
+
+protected:
+    bool ready;
+};
+
+class Console : public AbstractLogger
+{
+public:
+    Console() {}
+    void Init(const command_string_options_t &options) override
+    {
+        if (!options.contains(RUN_BACKGROUND_OPTION) && !options.contains(DAEMONIZE_OPTION))
+            ready = true;
+    }
+    void DeInit() override {} // console doesn't need any
+
+    void Write(const std::string &data) override
+    {
+        if (ready)
+            std::cerr << data << std::endl
+                      << std::flush;
+    }
+};
+
 class Log
 {
 
@@ -56,23 +101,100 @@ public:
     Log(Log &) = delete;
     Log &operator=(const Log &) = delete;
 
-    static void Mount(const std::string &mount_path, bool w);
-    static void Umount(const std::string &mount_path);
-    static void Error(const std::string &what);
-    static void Setup(const auto &options)
+    static void Mount(const std::string &mount_path, bool write_mount)
     {
-        ImplGet().ImplSetup(options);
+        auto &instance = ImplGet();
+        auto &data = instance.ImplPrepareLogData(mount_path, EventMount, write_mount);
+        instance.ImplLog(data);
+    }
+    static void Umount(const std::string &mount_path)
+    {
+        auto &instance = ImplGet();
+        auto &data = instance.ImplPrepareLogData(mount_path, EventUnmount);
+        instance.ImplLog(data);
+    }
+    static void Error(const std::string &what)
+    {
+        auto &instance = ImplGet();
+        auto &data = instance.ImplPrepareLogData(what, EventError);
+        instance.ImplLog(data);
+    }
+    static void Info(const std::string &what)
+    {
+        auto &instance = ImplGet();
+        auto &data = instance.ImplPrepareLogData(what, EventInfo);
+        instance.ImplLog(data);
+    }
+    static void Init(const auto &options)
+    {
+        ImplGet().ImplInit(options);
+    }
+
+    static void SuppressDeinit()
+    {
+        ImplGet().run_deinit = false; // suppress when stopped by particular signal
+    }
+
+    ~Log()
+    {
+        if (run_deinit)
+            ImplDeInit();
     }
 
 private:
-    Log() : use_stderr(true), use_logfile(false), use_syslog(false), use_mqueue(false){};
-
-    void ImplSetup(const auto &options)
+    enum ImplEventType
     {
-        use_stderr = !options.contains(RUN_BACKGROUND_OPTION) && !options.contains(DAEMONIZE_OPTION);
-        use_logfile = options.contains(LOG_FILE_OPTION);
-        use_syslog = options.contains(USE_SYSLOG_OPTION);
-        use_mqueue = options.contains(MQUEUE_NAME_OPTION);
+        EventMount,
+        EventUnmount,
+        EventError,
+        EventInfo
+    };
+
+    void ImplDeInit()
+    {
+        for (auto logger : loggers)
+            logger->DeInit();
+    }
+
+    std::string &ImplPrepareLogData(const std::string &data, enum ImplEventType event_type, bool write_mount_flag = false)
+    {
+        log_data.clear();
+        std::stringstream ss;
+        switch (event_type)
+        {
+        case EventMount:
+            ss << "MOUNT:" << (write_mount_flag ? "RW" : "RD") << ":";
+            break;
+        case EventUnmount:
+            ss << "UNMOUNT:";
+            break;
+        case EventError:
+            ss << "ERROR:";
+            break;
+        case EventInfo:
+            ss << "INFO:";
+            break;
+        default:
+            break;
+        }
+        ss << data;
+        log_data = ss.str();
+        return log_data;
+    };
+
+    Log() : run_deinit(true) {}
+
+    void ImplLog(auto &data)
+    {
+        for (auto logger : loggers)
+            logger->Write(data);
+    }
+
+    void ImplInit(const auto &options)
+    {
+        loggers = {new Console};
+        for (auto &logger : loggers)
+            logger->Init(options);
     }
 
     static Log &ImplGet()
@@ -81,18 +203,49 @@ private:
         return log;
     }
 
-    bool use_syslog;
-    bool use_mqueue;
-    bool use_logfile;
-    bool use_stderr;
-    const std::string log_file;
-    const std::string mqueue_name;
-};
+    auto &ImplGetLoggers() { return loggers; }
 
-typedef std::map<const std::string, const char *> command_string_options_t;
+    std::vector<AbstractLogger *> loggers;
+    std::string log_data;
+    bool run_deinit;
+};
 
 int main(const int argc, const char *argv[])
 {
+    umask(~(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) & 0777);
+
+    struct sigaction child_sigaction = {0};
+    child_sigaction.sa_flags = SA_NOCLDWAIT; // suppress zombie creations
+    child_sigaction.sa_sigaction = [](int s, siginfo_t *info, void *arg) {
+        (void)arg;
+        if (s == SIGHUP)
+            Log::SuppressDeinit();
+        Log::Info("Stopped by signal "s + std::to_string(s));
+        _exit(0); // terminate process
+    };
+
+    for (int i = 1; i <= 64; i++)
+        sigaction(i, &child_sigaction, NULL);
+
+    // cleanup
+    std::ifstream pid_file(PID_FILE, std::ios::in | std::ios::binary | std::ios::ate);
+    if (pid_file.is_open())
+    {
+        if (pid_file.tellg()) // non zero pid file
+        {
+            std::string pid_data;
+            pid_data.resize(pid_file.tellg());
+            pid_data.reserve(pid_file.tellg());
+            pid_file.seekg(std::ios::beg);
+            pid_file.read(pid_data.data(), pid_data.size());
+            pid_t pid = std::strtoul(pid_data.c_str(), NULL, 10);
+            if (pid) // pid parsed w/o errors
+                kill(pid, SIGHUP);
+        }
+        pid_file.close();
+        unlink(PID_FILE);
+    }
+
     // parse command string for options
     command_string_options_t options;
 
@@ -191,9 +344,47 @@ int main(const int argc, const char *argv[])
         return 1; // terminate with error if command string options parsing failed
     }
 
-    print_map(options);
+    // fork if required
+    if (options.contains(RUN_BACKGROUND_OPTION) || options.contains(DAEMONIZE_OPTION))
+    {
+        try
+        {
+            pid_t pid = fork();
+            if (pid < 0)
+                throw std::exception();
+            else if (pid > 0)
+            {
+                //parent: write pid file and exit
+                std::ofstream pid_file(PID_FILE, std::ios::out | std::ios::trunc);
+                if (pid_file.is_open())
+                {
+                    std::string pid_data = std::to_string(pid);
+                    pid_file.write(pid_data.data(), pid_data.size());
+                    pid_file.flush();
+                    pid_file.close();
+                }
+                return 0;
+            }
+            else
+            {
+                // child
+                fclose(stderr);
+                fclose(stdin);
+                fclose(stdout);
+                setsid();
+                while (true)
+                    sleep(1);
+            }
+        }
+        catch (const std::exception &e)
+        {
+            return 2;
+        }
+    }
 
-    Log::Setup(options);
+    // print_map(options);
+
+    Log::Init(options);
 
     return 0;
 }
